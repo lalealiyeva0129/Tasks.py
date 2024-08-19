81fe69b030083af3a5a5d993741e087689f26acd
lalealiyeva0129 <lalealiyeva744@gmail.com>
2024-08-19 19:43:14 +0400

    first commit

--- quiz.py
+++ quiz.py
@@ -1,37 +1,35 @@
 #alizadelale
-#task1
+print("task2")
 #  1
 #  1 2
 #  1 2 3
 #  1 2 3 4
-#  1 2 3 4 5  
-"""print("Sual1") 
+#  1 2 3 4 5   
 for i in range(1,6):
     for j in range(1,i+1):
         print(j,end=(" "))
     print("")
     
-#task2
+print("task3")
 # Istifadeciden eded qebul edib, 1 den bu edede qeder olan elementlerin cemini yazdirmaq.
+x=int(input("Zehmet olmasa ededi daxil edin:"))    
+print(x)
+for i in range(1,x+1):
+    if i>0:
+       s=(((x+1)*x)/2)
+print("Cem=",s)
 
-#task3
+print("task4")
 #Verilmis ededin vurma cedvelin qurun.
-print("Sual3")
 n=7
 for i in range(1,11,1):
     x=i*n
     print(x)
-# istifadeci terfinden eded daxil et, onun vurma cedvelin qur.   
-eded=int(input("Ededi daxil edin:"))
-for i in range(1,eded+1):
-    i=i*2
-    print(i)
 
-#task4 
+print("task5")
 # Nömrə beşə bölünməlidir
  #Əgər rəqəm 150-dən çox olarsa, onu keçin və növbəti nömrəyə keçin
  #Əgər rəqəm 500-dən çox olarsa, döngəni dayandırın 
-print("Sual4")
 numbers = [12, 75, 150, 180, 145, 525, 50]
 for i in numbers:
     if i%5==0:
@@ -39,9 +37,27 @@ for i in numbers:
             continue
             if numbers<500:
                 break
-        print(i) 
-        """
-#task5
+        print(i)
+ 
+print("task6")
+#Bir ədəddəki rəqəmlərin ümumi sayını hesablayın
+y=input("Zehmet olmasa ededi daxil edin:")
+for i in (y):
+    print(int(i))
+    
+print("task7")
+#Aşağıdakı nümunəni çap edin
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=(" "))
+    print("")
+    
+print("task8")
 #list1=[10,20,30,40,50] donguden istifade ederek  siyahini ters qaydada cap edin.
 list1=[10,20,30,40,50]
 list2=[]
@@ -52,16 +68,33 @@ for i in list1:
     for x in list2:
         print(x)
 
-#task6
-# For döngəsinin uğurla icrasından sonra “Bitti” mesajını göstərmək üçün else blokundan istifadə edin.   
+print("task9")
+#for döngəsindən istifadə edərək -10-dan -1-ə qədər rəqəmləri göstərin   
+for i in range(-10,0,1):
+    print(i)
+    
+print("task10 ") 
+# For döngəsinin uğurla icrasından sonra “Bitti” mesajını göstərmək üçün else blokundan istifadə edin.
 for i in range(5):
     print(i)
 else:
     print("Bitdi!")
-#task7
+    
+print("task11") 
 #Bir diapazonda bütün sadə ədədləri göstərmək üçün proqram yazın
 print("25 ile 50 arasinda olan sade ededler:")   
 for i in range(25,50):
     if i%2!=0 and i%3!=0 and i%7!=0 and i%5!=0:
         print(i)
+
     
+print("task12")
+#Fibonacci seriyasını 10 terminə qədər göstərin:0 1 1 2 3 5 8 13 21 34
+x=0
+y=1
+for i in range(5,-1):
+    n=i*(i+1) 
+print(n)
+
+
+
